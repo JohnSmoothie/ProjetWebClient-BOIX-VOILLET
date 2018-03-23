b8b61e4612b9da3df87fd70ea585a355698ac846
E164708F <nostale789>
2018-03-23 10:25:16 +0100

    r

--- script/index.js
+++ script/index.js
@@ -2,28 +2,29 @@ $(document).ready(function() {
   var urlFlickr = "https://api.flickr.com/services/rest/";
   var apiKey = "45074180ed9c766da6cdd745043f1cdc";
 
-  var form = $("#form");
-  var ville = $("#ville");
-  var nbPhotos = $("#nbPhotos");
 
 
-  $('table').hide();
+
+  $('#VueTab').hide();
   var lastTab = null;
   $(".onglets a").each(function(index, elt) {
-      $(elt).click(function(event) {
-          event.preventDefault();
-          $('table').hide();
-          var currentTab = $('table'+$(this).attr("href"));
-          if(currentTab == lastTab) {
-              currentTab.hide();
-          } else {
-              currentTab.show();
-              lastTab = currentTab;
-          }
-      })
+    $(elt).click(function(event) {
+      event.preventDefault();
+      $('table').hide();
+      var currentTab = $('table'+$(this).attr("href"));
+      if(currentTab == lastTab) {
+        currentTab.hide();
+      } else {
+        currentTab.show();
+        lastTab = currentTab;
+      }
+    })
   });
 
+  var form = $("#form");
   $(form).submit(function(event){
+    var ville = $("#ville");
+    var nbPhotos = $("#nbPhotos");
     event.preventDefault();
     console.log($(nbPhotos).val());
     console.log($(ville).val());
@@ -36,18 +37,24 @@ $(document).ready(function() {
         api_key : apiKey,
         tags : $(ville).val(),
         format : 'json',
-        par_page : $(nbPhotos).val(),
+        per_page : $(nbPhotos).val(),
         nojsoncallback : 1
       }
     });
-
+    
     ajax.done(function(res){
       console.log(res);
       $("#listePhoto").empty();
       $(res.photos.photo).each(function(index, elt){
         console.log(this);
         var lien = "https://farm"+this.farm+".staticflickr.com/"+this.server+"/"+this.id+"_"+this.secret+".jpg";
-        $("#listePhoto").append("<li><img src=\""+lien+"\"/></li>");
+        $("#listePhoto").append("<li><img id=\""+this.id+"\"src=\""+lien+"\"/></li>");
+        var photo = $("#"+this.id);
+        $(photo).click(function() {
+          $(photo).closest(".modal").css("display", "initial");
+          $(photo).closest(".modalbg").css("display", "initial");
+        });
+
       });
     });
 
@@ -56,59 +63,71 @@ $(document).ready(function() {
       console.log(data);
     });
 
-    });
+  });
 
-    var inputCommune = $("#ville");
+  var inputCommune = $("#ville");
 
-    console.log($("#ville"));
+  console.log($("#ville"));
 
-    $("#ville").autocomplete({
+  $("#ville").autocomplete({
 
-      source: function(request, response){
+    source: function(request, response){
 
-        recherche = 'commune='+inputCommune.val()+"&maxRows=10";
-        console.log(recherche);
+      recherche = 'commune='+inputCommune.val()+"&maxRows=10";
+      console.log(recherche);
 
-        var ajax = $.ajax({
-          url : 'http://infoweb-ens/~jacquin-c/codePostal/commune.php',  // Ressource ciblée coté serveur
-          type : 'GET',
-          data : recherche,//$_GET['nom'] au niveau serveur
+      var ajax = $.ajax({
+        url : 'http://infoweb-ens/~jacquin-c/codePostal/commune.php',  // Ressource ciblée coté serveur
+        type : 'GET',
+        data : recherche,//$_GET['nom'] au niveau serveur
+      });
+
+      ajax.done(function(codeHtmlSucces){
+        console.log(codeHtmlSucces);
+        map = $.map(codeHtmlSucces, function(n, i){
+          ville = n.Ville;
+          label = n.Ville;
+          value = n.Ville;
+          return {ville, label, value};
         });
+        console.log(map);
+        return response(map);
+      });
+    }
 
-        ajax.done(function(codeHtmlSucces){
-          console.log(codeHtmlSucces);
-            map = $.map(codeHtmlSucces, function(n, i){
-              ville = n.Ville;
-              label = n.Ville;
-              value = n.Ville;
-              return {ville, label, value};
-            });
-            console.log(map);
-            return response(map);
-          });
-      }
+  });
 
-    });
+  $("#ville").autocomplete( "option", "minLength", 3 );
 
-    $("#ville").autocomplete( "option", "minLength", 3 );
+  $("#ville").on( "autocompleteselect", function( event, ui ) {
+    event.preventDefault();
 
-    $("#ville").on( "autocompleteselect", function( event, ui ) {
-      event.preventDefault();
+    console.log(ui);
+    console.log(ui.item.label);
+    console.log(ui.item.value);
+    console.log($(this));
 
-          console.log(ui);
-          console.log(ui.item.label);
-          console.log(ui.item.value);
-          console.log($(this));
-
-          inputCommune.val(ui.item.ville);
+    inputCommune.val(ui.item.ville);
 
 
-    });
+  });
 
-    /*loupe
-    $("td.tab-chiffre").each(function(index, elt) {
-        $(elt).mouseenter(function(){
-            $('#loupe').text($(this).text());
-        })
-    })*/
+
+
+  $(".modal_close").click(function() {
+    $(".modal").css("display", "none");
+    $(".modalbg").css("display", "none");
+  });
+
+  $(".modalbg").click(function() {
+    $(".modal").css("display", "none");
+    $(".modalbg").css("display", "none");
+  });
+
+  /*loupe
+  $("td.tab-chiffre").each(function(index, elt) {
+  $(elt).mouseenter(function(){
+  $('#loupe').text($(this).text());
+})
+})*/
 });
